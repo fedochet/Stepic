e49ec3c8fb544874ee2acaab2c8086912631f051
fedochet <fedochet@yandex.ru>
2014-10-10 22:18:41 +0400

    Класс множеств + екзе

--- uncrossed.cpp
+++ uncrossed.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <vector>
 #include <string>
 
 using namespace std;
+
+class uncrossed_groups{
+
+public:
+	vector<int> base;
+
+	uncrossed_groups(){};
+
+	// заполняет вектор номерами ячеек, т.е. ссылками на самих себя
+	uncrossed_groups(int const &a){
+		for (int i = 0; i++; i<a)
+			base.push_back(i);
+	}
+
+	~uncrossed_groups(){};
+
+};
 
 int main()
 {
